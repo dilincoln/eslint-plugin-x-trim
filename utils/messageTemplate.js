d759a817af530f467feac2878e19181f90634443
Diego Lincoln <diego.lincoln@hotmail.com>
2023-09-25 05:11:03 -0300

    chore: update message template for trimmed values

--- utils/messageTemplate.js
+++ utils/messageTemplate.js
@@ -1,5 +1,5 @@
 const messageTemplate = (tagName, parsed) => {
-	return `Attribute of tag "${tagName}" should be trimmed to "${parsed}".`
+	return `Value of tag "${tagName}" should be trimmed to "${parsed}".`
 }
 
 module.exports = messageTemplate
